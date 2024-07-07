a162a1ad77066a6afc3a8aa6bc04ce3e68ab8f26
nicolasduernbeck <nicolas.duernbeck@gmx.at>
2024-07-07 17:24:14 +0200

    new one-liner

--- main.ps1
+++ main.ps1
@@ -3,3 +3,7 @@ Set-Location .\Users\nicol\Desktop\Coding\PowerShell\samples
 Get-ChildItem | Where-Object {$_.Extension -eq '.txt'}
 
 Get-Process | Where-Object {$_.CPU -gt 20} | Sort-Object {$_.Id}
+
+Get-ChildItem | ForEach-Object {$_.CreationTime} | Format-List
+
+Get-Process | Where-Object {$_.id -gt 4000} | ForEach-Object {$_.CPU} | Measure-Object -Average
